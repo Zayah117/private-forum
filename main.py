c9527afe4bab5b1042800ec09bacf321d286af83
Zayah117 <isaiah.baker@simpledove.com>
2017-07-10 07:12:55 -0400

    Add test-post.html and style.css

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Post
+
+app = Flask(__name__, static_url_path='/static/*')
 
 @app.route('/')
 def hello_world():
 	return render_template('front.html')
+
+@app.route('/test')
+def post_test():
+	return render_template('test-post.html', items=None)
